fcf845546ab7d41194e4740db524f1aa87fe36b2
Mythili0724 <mythilidayalan@gmail.com>
2025-07-09 20:56:50 +0530

    Add files via upload
    day1

---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+  byte age=35;
+  System.out.println(age);
+  short ageOfOldman=100;
+  System.out.println(ageOfOldman);
+  int salary=25000;
+  System.out.println(salary);
+  long phoneNumber=9840502490L;
+  System.out.println(phoneNumber);
+  char initial='D';
+  System.out.println(initial);
+  float emi=10.45f;
+  System.out.println(emi);
+  double val=12.345678956;
+  System.out.println(val);
+  
+  boolean name1= true;
+  
+  String name="Mythili.D";
+  System.out.println("My name is"+ " " +name+ " "+name1);
+
+	}
+
+}
